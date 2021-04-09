2db64eea77c89d30cf32618bc80a16fcf44c5b80
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2021-04-10 00:18:50 +0200

    First deploy

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -16,13 +17,14 @@ func getRestrictions(w http.ResponseWriter, r *http.Request) {
 	lang := mux.Vars(r)["lang"]
 	com := mux.Vars(r)["com"]
 
-	http.ServeFile(w, r, "./../../static/"+lang+"/"+com+".json")
+	http.ServeFile(w, r, "./static/"+lang+"/"+com+".json")
 }
 
 func main() {
+	port := os.Getenv("PORT")
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", indexRoute).Methods("GET")
 	router.HandleFunc("/{com}/{lang}", getRestrictions).Methods("GET")
-	log.Fatal(http.ListenAndServe(":3000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
